1a322b0a0107492283e7e9ad85a0231cc0f0dad1
GinoGallina <ginogallina2002@gmail.com>
2023-10-18 18:10:45 -0300

    fix id

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,21 +8,10 @@ export default defineConfig({
     // Enable React to support React JSX components.
     integrations: [
         react(),
+        
     ],
     extensions: ['.astro', '.jsx'],
-    middleware: [
-        (req, res, next) => {
-            // Si la solicitud es para el servidor, pasa al servidor
-            if (req.url.startsWith("/server/")) {
-                return next();
-            }
-
-            // De lo contrario, sirve la aplicación de Astro
-            return renderHtml();
-        },
-    ],
     output: 'server',
     adapter: vercel(),
 });
 
-export const apiPath = "/api";
